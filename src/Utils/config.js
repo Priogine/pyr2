2984f290dd6f275dafe58027a87a0d83773d062d
Priogine <104873315+Priogine@users.noreply.github.com>
2022-05-29 16:48:57 +0200

    Update config.js

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -6,7 +6,7 @@ import {DEFAULT_NETWORK} from './network';
 export const config = {
     networkId: DEFAULT_NETWORK,
     bananaAbi: bananaAbi.default,
-    bananaAddress: "0x3e95540C9e64FCaC38C1B10a6045e3E8c55ac139",
+    bananaAddress: "xthfv",
     nodeAbi: nodeAbi.default,
     nodeAddress: "0xe44e2912BA5c0085B621EAEcB10c7e9e3dBE97f9",
     LPAbi: LPAbi.default,
